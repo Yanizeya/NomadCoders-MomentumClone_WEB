40bce311a4bdfab6d211b92fdcfce0849d04769a
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-06-10 23:38:09 +0900

    Add files via upload

--- Momentum-Upgrade/script.js
+++ Momentum-Upgrade/script.js
@@ -1,3 +1,27 @@
 const title = document.getElementById("title");
 
-console.log(title);
+function handleTitleClick(){
+    if(title.style.color !== "blue"){
+        title.style.color = "blue";
+    } else{
+        title.style.color = "black";
+    }
+}
+
+function handleMouseEnter(){
+    title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
