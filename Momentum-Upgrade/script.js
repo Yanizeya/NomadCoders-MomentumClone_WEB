1c3f0d02472788a2eaecc44c7f1aa12973d0793e
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-06-11 23:09:01 +0900

    Add files via upload

--- Momentum-Upgrade/script.js
+++ Momentum-Upgrade/script.js
@@ -1,15 +1,9 @@
 const h1 = document.querySelector("h1");
 
 function handleH1Click(){
-    const currentClass = h1.className;
-    let newClass;
-    const clickedClass = "active"
-    if(currentClass !== clickedClass){
-        newClass = clickedClass;
-    } else{
-        newClass = "";
-    }
-    h1.className = newClass;
+    h1.classList.toggle("active");
 }
 
 h1.addEventListener("click", handleH1Click);
+
+
