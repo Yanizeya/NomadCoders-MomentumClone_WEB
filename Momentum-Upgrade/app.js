87479ca91944e5d3192b9df76aad2e868cf1310c
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-06-13 23:25:01 +0900

    Add files via upload

--- Momentum-Upgrade/app.js
+++ Momentum-Upgrade/app.js
@@ -2,13 +2,9 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginButton(){
-    const value = loginInput.value;
-    if(value === ""){
-        alert("Please write your name")
-    }else if(username.length > 15){
-        alert("Your name is too long");
-    }
+function onLoginButton(potato){
+    potato.preventDefault();
+    console.log(potato);
 }
 
-loginButton.addEventListener("click", onLoginButton);
+loginButton.addEventListener("submit", onLoginButton);
