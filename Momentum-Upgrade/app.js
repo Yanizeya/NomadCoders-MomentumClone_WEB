cc4a11758cff261a3dbe7615176265aa248600f7
Yanizeya <43463709+Yanizeya@users.noreply.github.com>
2021-06-15 23:54:03 +0900

    Add files via upload

--- Momentum-Upgrade/app.js
+++ Momentum-Upgrade/app.js
@@ -1,5 +1,6 @@
- const loginForm = document.getElementById("login-form");
+const loginForm = document.getElementById("login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
 function onLoginSubmit(potato){
     potato.preventDefault();
@@ -7,4 +8,11 @@ function onLoginSubmit(potato){
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event); 
+    alert("click!");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
